872b8903c3333199bed0b95f06e9ecd8ba23e0e9
zakarya kartti <kartti.zakarya@gmail.com>
2019-12-08 16:21:02 +0100

    Update devise initializer

--- devise.rb
+++ devise.rb
@@ -33,7 +33,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'contact@tbibcom.com'
+  config.mailer_sender = 'TODO_REPLACE_WITH_NAME@DOMAIN.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
